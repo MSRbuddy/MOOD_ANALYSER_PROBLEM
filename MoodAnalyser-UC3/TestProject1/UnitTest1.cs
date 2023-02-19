7541c37dce0847b524fe5ed4025c36606e683cc0
MSRbuddy <meghanadasari222@gmail.com>
2023-02-19 15:01:54 +0530

    UseCase-3 is added!

---
+++ MoodAnalyser-UC3/TestProject1/UnitTest1.cs
@@ -0,0 +1,42 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MoodAnalyzer_UC3;
+
+namespace MoodAnalyzer_UC3
+{
+    [TestClass]
+    public class MoodAnlyzerTest
+    {
+
+        [TestMethod]
+        public void Given_NULL_Mood_Should_Throw_MoodAnalysisException()
+        {
+            try
+            {
+                string message = null;
+                MoodAnalyse moodAnalyse = new MoodAnalyse(message);
+                string mood = moodAnalyse.AnalyseMood();
+            }
+            catch (MoodAnalysisException e)
+            {
+                Assert.AreEqual("Mood should not be null", e.Message);
+            }
+        }
+
+        [TestMethod]
+        public void Given_Empty_Mood_Should_Throw_MoodAnalysisException_Indicating_EmptyMood()
+        {
+            try
+            {
+                string message = "";
+                MoodAnalyse moodAnalyse = new MoodAnalyse(message);
+                string mood = moodAnalyse.AnalyseMood();
+            }
+            catch (MoodAnalysisException e)
+            {
+                Assert.AreEqual("Mood should not be Empty", e.Message);
+            }
+        }
+    }
+}
+
+
